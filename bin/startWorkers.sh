028cd2d0bd8ccc980efb5659b86e56d63e55d0c0
lingzhong <ling.j.zhong@gmail.com>
2015-03-31 22:43:59 -0400

    added .sh scripts and makefile

---
+++ bin/startWorkers.sh
@@ -0,0 +1,11 @@
+ZOOBIN="${BASH_SOURCE-$0}"
+ZOOBIN="$(dirname "${ZOOBIN}")"
+ZOOBINDIR="$(cd "${ZOOBIN}"; pwd)"
+
+if [ -e "$ZOOBIN/../libexec/zkEnv.sh" ]; then
+  . "$ZOOBINDIR"/../libexec/zkEnv.sh
+else
+  . "$ZOOBINDIR"/zkEnv.sh
+fi
+
+"$JAVA" -cp ../scheduler/src:"$CLASSPATH" Worker localhost:2181 $1
